4fcdc78764d28527286d47deefc4ce3e4f1da722
mrsrobot <44340262+mrsrobot@users.noreply.github.com>
2018-10-21 16:21:49 +0200

    Create py.py

---
+++ py.py
@@ -0,0 +1,34 @@
+1) Add kali linux repositories
+2) Update
+3) Remove all kali linux repositories
+4) View the contents of sources.list file
+					''')
+					repo = raw_input("\033[1;32mWhat do you want to do ?> \033[1;m")
+					if repo == "1":
+						cmd1 = os.system("apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6")
+						cmd2 = os.system("echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
+					elif repo == "2":
+						cmd3 = os.system("apt-get update -m")
+					elif repo == "3":
+						infile = "/etc/apt/sources.list"
+						outfile = "/etc/apt/sources.list"
+
+						delete_list = ["# Kali linux repositories | Added by Katoolin\n", "deb http://http.kali.org/kali kali-rolling main contrib non-free\n"]
+						fin = open(infile)
+						os.remove("/etc/apt/sources.list")
+						fout = open(outfile, "w+")
+						for line in fin:
+						    for word in delete_list:
+						        line = line.replace(word, "")
+						    fout.write(line)
+						fin.close()
+						fout.close()
+						print ("\033[1;31m\nAll kali linux repositories have been deleted !\n\033[1;m")
+					elif repo == "back":
+						inicio1()
+					elif repo == "gohome":
+						inicio1()
+					elif repo == "4":
+						file = open('/etc/apt/sources.list', 'r')
+
+print (file.read())
